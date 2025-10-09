4c659cb68e24f0f0dad1613b86f3af9866225257
DJCoder311 <166066300+DJCoder311@users.noreply.github.com>
2025-10-09 19:43:26 -0400

    Update LinkedListCycleAnalyzer.java
    Completed Assignment

--- StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
+++ StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
@@ -6,6 +6,33 @@ import support.LLNode;
 
 public class LinkedListCycleAnalyzer<T> {
     public static <T> CycleInfo detectCycleInfo(LLNode<T> head){
-        throw new NotImplementedException("TODO: implement this method");
+
+        LLNode<T> pointer1 = head;
+        LLNode<T> pointer2 = head;
+
+        do {
+            if (pointer1.getLink() == null || pointer2.getLink() == null)
+            {return new CycleInfo(-1, 0);}
+            pointer1 = pointer1.getLink();
+            pointer2 = pointer2.getLink().getLink();
+        }
+        while (pointer1!=pointer2);
+
+        int entryIndex = 0;
+        pointer1 = head;
+        while(pointer1 != pointer2){
+            pointer1 = pointer1.getLink();
+            pointer2 = pointer2.getLink();
+            entryIndex+=1;
+        }
+
+        int cycleLength = 1;
+        pointer1 = pointer2.getLink();
+        while(pointer1 != pointer2){
+            pointer1 = pointer1.getLink();
+            cycleLength+=1;
+        }
+
+        return new CycleInfo(entryIndex,cycleLength);
     }
 }
